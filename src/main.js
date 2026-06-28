52dba93bbf65aaae42ecdb023a220cc445eeae87
simka <semirenkonasta81@gmail.com>
2026-06-28 22:03:40 +0300

    complete image search app with fixed izitoast imports

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import iziToast from 'iziToast';
-import 'iziToast/dist/css/iziToast.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 import { getImagesByQuery } from './js/pixabay-api.js';
 import {
